234768814252322fb76841d3485abf16e9117846
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-12-11 16:18:41 +0530

    Add files via upload

---
+++ 10themeSwitcher/src/contexts/Theme.js
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    lightTheme: () => {},
+    darkTheme: () => {}
+})
+
+
+export const ThemeProvider = ThemeContext.Provider
+
+
+export default function useTheme() {
+    return useContext(ThemeContext)
+}
